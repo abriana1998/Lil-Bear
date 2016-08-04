b1fac537d79f06422378b264a6173258b7b2ce74
abriana1998 <a.briana1998@gmail.com>
2016-08-04 09:44:26 -0400

    heywhatsup

---
+++ sketch.js
@@ -0,0 +1,57 @@
+function square (x,y,side) { 
+  rect(x,y,side,side)
+  
+}
+  
+  function circle (x,y,diameter) {
+ ellipse (x,y,w,h);
+  }
+
+
+function setup() {
+  createCanvas(640,480);
+  
+  stroke(191,128,64)
+  fill(191,128,64)
+  ellipse (350,360,100);
+  
+  stroke(191,128,64)
+  fill(191,128,64)
+  ellipse (315,320,80)
+  
+    stroke(191,128,64)
+  fill(191,128,64)
+  ellipse (385,315,75)
+  
+  stroke(255,204,204)
+  fill(255,204,204)
+  ellipse(315,315,40)
+  
+  stroke(255,204,204)
+  fill(255,204,204)
+  ellipse(385,315,40)
+  
+  stroke(255,255,255)
+  fill(255,255,255)
+  ellipse(367,360,20)
+  
+  stroke(255,255,255)
+  fill(255,255,255)
+  ellipse(338,360,20)
+  
+  stroke(0,0,0)
+  fill(0,0,0)
+  ellipse(338,360,7)
+  
+  stroke(0,0,0)
+  fill(0,0,0)
+  ellipse(368,360,7)
+  
+  stroke(255,242,230)
+  fill(255, 242,230)
+  ellipse(352,375,12)
+}
+
+function draw() {
+  
+}
